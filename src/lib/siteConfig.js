387fcb0c1e71b6d929ea4dda35d28b142339e684
Jeff Gostick <jgostick@gmail.com>
2023-01-04 14:12:54 -0500

    readding youtube link for now

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -6,6 +6,7 @@ export const SITE_DESCRIPTION = "Personal blog site of Jeff Gostick";
 export const DEFAULT_OG_IMAGE =
 	'https://user-images.githubusercontent.com/6764957/147861359-3ad9438f-41d1-47c8-aa05-95c7d18497f0.png';
 export const MY_TWITTER_HANDLE = 'porespy';
+export const MY_YOUTUBE = 'https://youtube.com/swyxTV';
 export const POST_CATEGORIES = ['Blog']; // Other categories you can consider adding: Talks, Tutorials, Snippets, Podcasts, Notes...
 export const GH_PUBLISHED_TAGS = ['Published'];
 
